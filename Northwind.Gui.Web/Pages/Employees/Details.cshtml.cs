e033ff0b1598facee8179cd38627a10348cbcf41
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2019-08-27 11:34:51 +0200

    initial

---
+++ Northwind.Gui.Web/Pages/Employees/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Entities.Models;
+
+namespace Northwind.Gui.Web.Pages.Employees
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Northwind.Entities.Models.NorthwindContext _context;
+
+        public DetailsModel(Northwind.Entities.Models.NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public Northwind.Entities.Models.Employees Employees { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employees = await _context.Employees
+                .Include(e => e.ReportsToNavigation).FirstOrDefaultAsync(m => m.EmployeeId == id);
+
+            if (Employees == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Northwind.Gui.Web/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Entities.Models;
+
+namespace Northwind.Gui.Web.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Northwind.Entities.Models.NorthwindContext _context;
+
+        public IndexModel(Northwind.Entities.Models.NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Northwind.Entities.Models.Employees> Employees { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Employees = await _context.Employees
+                .Include(e => e.ReportsToNavigation).ToListAsync();
+        }
+    }
+}
