87761b36cc4af36c20d9c8f85497c0dfa97a2a54
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2019-09-18 10:37:30 +0200

    Added comments to OrderService

--- Northwind.DataAcess/OrderService.cs
+++ Northwind.DataAcess/OrderService.cs
@@ -17,7 +17,9 @@ namespace Northwind.DataAcess
         private IGenericRepository<Customer> _customers;
         private IGenericRepository<Shipper> _shippers;
         #region properties
-        //instantiates a new repository if there isn't one already.
+        /// <summary>
+        /// Gets Order repository. instantiates a new repository if there isn't one already.
+        /// </summary>
         public IGenericRepository<Order> Orders
         {
             get
@@ -30,7 +32,9 @@ namespace Northwind.DataAcess
                 return _orders;
             }
         }
-
+        /// <summary>
+        /// Gets Customer repository. instantiates a new repository if there isn't one already.
+        /// </summary>
         public IGenericRepository<Customer> Customers
         {
             get
@@ -43,7 +47,9 @@ namespace Northwind.DataAcess
                 return _customers;
             }
         }
-
+        /// <summary>
+        /// Gets Shipper repository. instantiates a new repository if there isn't one already.
+        /// </summary>
         public IGenericRepository<Shipper> Shippers
         {
             get
@@ -56,6 +62,9 @@ namespace Northwind.DataAcess
                 return _shippers;
             }
         }
+        /// <summary>
+        /// Gets OrderDetails repository. instantiates a new repository if there isn't one already.
+        /// </summary>
         public IGenericRepository<OrderDetail> OrderDetails
         {
             get
@@ -69,11 +78,20 @@ namespace Northwind.DataAcess
             }
         }
         #endregion
+        /// <summary>
+        /// Adds an Order to the database
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         public async Task<int> AddOrder(Order order)
         {
             return await Orders.Add(order);
         }
-
+        /// <summary>
+        /// Deletes an Order from the database
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         public async Task DeleteOrder(Order order)
         {
             var orderDetails = await OrderDetails.List(o => o.OrderId == order.Id);
@@ -81,39 +99,63 @@ namespace Northwind.DataAcess
             await OrderDetails.DeleteRange(orderDetails);
             await Orders.Delete(order);
         }
-
+        /// <summary>
+        /// Gets an Order by the id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<Order> GetById(int id)
         {
             return await Orders.GetById(id, new string[4] { "Employee", "Customer", "ShipViaNavigation", "OrderDetails.Product" });
         }
-
+        /// <summary>
+        /// Gets all customers
+        /// </summary>
+        /// <returns></returns>
         public async Task<IList<Customer>> GetCustomers()
         {
             return await Customers.List();
         }
-
+        /// <summary>
+        /// Gets the next 25 upcomming shipments.
+        /// </summary>
+        /// <returns></returns>
         public async Task<IList<Order>> GetNextShipments()
         {
             IList<Order> list = await Orders.List(new string[3] { "Employee", "Customer", "ShipViaNavigation" });
             return list.Cast<Order>().Where(o => o.ShippedDate == null).OrderBy(d => d.RequiredDate).Take(25).ToList();
         }
-
+        /// <summary>
+        /// Gets all Orders
+        /// </summary>
+        /// <returns></returns>
         public async Task<IList<Order>> GetOrders()
         {
             
             return await Orders.List(new string[3] {"Employee","Customer","ShipViaNavigation"});
         }
-
+        /// <summary>
+        /// Gets all Shippers
+        /// </summary>
+        /// <returns></returns>
         public async Task<IList<Shipper>> GetShippers()
         {
             return await Shippers.List();
         }
-
+        /// <summary>
+        /// Updates the Order in the database
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         public async Task<int> UpdateOrder(Order order)
         {
             return await Orders.Edit(order);
         }
-
+        /// <summary>
+        /// Gets the invoices views from the database by the OrderId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IList<Invoice>> GetInvoice(int id)
         {
             return await context.Invoices.Where(i => i.OrderId == id).ToListAsync();
